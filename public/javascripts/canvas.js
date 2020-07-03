5e965065cd0726dc264a8ac0112a6b290809be2d
Ken King <kking935@vt.edu>
2020-07-03 18:34:27 -0400

    removed card types

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -212,7 +212,7 @@ function drawCard(card, position, scale) {
 	ctx.font = "bold " + (64 * scale * r) + "px chinese_takeaway";
 	ctx.fillText(card.power, position.x + cardWidth * scale / 2, position.y + cardHeight * scale * 0.4);
 	ctx.font = (32 * scale * r) + "px Arial";
-	ctx.fillText(types[card.type], position.x + cardWidth * scale / 2, position.y + cardHeight * scale * 0.7);
+	// ctx.fillText(types[card.type], position.x + cardWidth * scale / 2, position.y + cardHeight * scale * 0.7);
 }
 
 function drawPointCard(card, position, scale) {
@@ -228,10 +228,10 @@ function drawPointCard(card, position, scale) {
 	ctx.strokeRect(position.x, position.y, cardWidth * scale, cardWidth * scale);
 	ctx.fillStyle = typeColors[card.type];
 	ctx.font = "bold " + (72 * scale * r) + "px Arial";
-	ctx.fillText(types[card.type][0], position.x + cardWidth * scale / 2, position.y + cardWidth * scale * 0.5);
+	// ctx.fillText(types[card.type][0], position.x + cardWidth * scale / 2, position.y + cardWidth * scale * 0.5);
 	ctx.strokeStyle = "#ffffff";
 	ctx.lineWidth = 3 * r * scale;
-	ctx.strokeText(types[card.type][0], position.x + cardWidth * scale / 2, position.y + cardWidth * scale * 0.5);
+	// ctx.strokeText(types[card.type][0], position.x + cardWidth * scale / 2, position.y + cardWidth * scale * 0.5);
 }
 
 function drawUnknownCard(position, scale) {
@@ -311,7 +311,7 @@ var clickCursor = false,
 	labels = [],
 	labelFont = "RagingRedLotusBB";
 var typeColors = ["#FF8B26", "#1260E6", "#74D5F2"];
-var types = ["Fire", "Water", "Ice"];
+// var types = ["Fire", "Water", "Ice"];
 var colors = {"yellow": "#fdee00", "orange": "#ffb235", "green": "#52a546", "blue": "#246acd", "red": "#e02929", "purple": "#9738af"};
 
 init();
