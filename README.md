82ec56f19e12d7dd51b1fca56313c1093cf7e197
kking935 <kking935@vt.edu>
2020-11-15 00:30:44 -0500

    added types!

--- README.md
+++ README.md
@@ -19,3 +19,30 @@ I plan to add my own explaination of the rules based on how my family plays the 
 
 ### Credits
 The code for this game was built off code from another project called [Card-Fu](https://github.com/tobloef/card-fu) developed by [@tobloef](https://github.com/tobloef)
+
+
+# Node Typescript
+A base app for developing node servers with typescript
+## Development
+To start for development:
+```
+# with npm
+npm run dev
+
+# or with yarn
+yarn dev
+```
+
+During development, write your typescript code in the `app.ts` file.
+
+## Production
+To build for production:
+```
+# with npm
+npm run build
+
+# or with yarn
+yarn build
+```
+
+This will compile the typescript code into javascript and add it to the `dist` folder
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import * as dotenv from 'dotenv';
+const app: Application = express();
+
 // This file starts both the Express server, used to serve the
 // actual webpage, and the Socket.io server, used to handle
 // the realtime connection to the client.
 
-var express = require("express");
-var app = express();
 var http = require("http").Server(app);
 var io = require("./libs/game_manager").listen(http);  // Start Socket.io server and let game_manager handle those connections
 
-app.set("port", (process.env.PORT || 3001));  // Use either given port or 3001 as default
+// Configure dotenv
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
 app.use(express.static("public"));  // Staticly serve pages, using directory 'public' as root 
 
 // User connects to server
@@ -21,7 +27,6 @@ app.get("*", function(req, res) {
 	res.status(404).send("Error 404 - Page not found");
 });
 
-// Start http server
-http.listen(app.get("port"), function() {
-	console.log("Node app started on port %s", app.get("port"));
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
